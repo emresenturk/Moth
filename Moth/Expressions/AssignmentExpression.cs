e5efd5c3fcbae7ccdc8fb8da1e5da9fbe9d2db05
Emre Senturk <m.emre.senturk@gmail.com>
2015-03-21 02:08:57 +0200

    Assignment expression added.

---
+++ Moth/Expressions/AssignmentExpression.cs
@@ -0,0 +1,9 @@
+﻿namespace Moth.Expressions
+{
+    public class AssignmentExpression : IQueryExpression
+    {
+        public IQueryExpression Destination { get; set; }
+
+        public IQueryExpression Source { get; set; }
+    }
+}
